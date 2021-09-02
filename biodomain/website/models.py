9f6258c76a2bef3ebac048e40142888fada1cd5a
Ridhima <54638081+RidhimaKohli@users.noreply.github.com>
2021-09-03 00:09:55 +0530

    display manual database , admin

--- biodomain/website/models.py
+++ biodomain/website/models.py
@@ -1,3 +1,16 @@
+from os import name
 from django.db import models
 
 # Create your models here.
+# equipment name
+# category
+# institute
+# City/region
+
+
+class Equipments(models.Model):
+    e_name = models.CharField(max_length=30)
+    category = models.CharField(max_length=15)
+    institute = models.CharField(max_length=50)
+    city = models.CharField(max_length=15)
+    
