0c38ad8aca51b9c62b478ec6db85a3d1559b4001
Palak Totla <totla.1@iitj.ac.in>
2021-12-13 23:48:07 +0530

    contact form

--- biodomain/website/urls.py
+++ biodomain/website/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import views
 
-
-app_name = "main"
-
 urlpatterns = [
     path('', views.home ,name='home'),
     path('instruments', views.InstrumentView,name='instrumentlistpage'),
